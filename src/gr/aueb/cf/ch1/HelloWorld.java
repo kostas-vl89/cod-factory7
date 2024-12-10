c391a4ab362dd9c1da6ed36df5814d3a346db3d9
Kostas Vlachos <kostas-vl@hotmail.com>
2024-12-10 20:02:27 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch1;
+/**
+ *Eκτυπώνει 'Hello World' στο standar output
+ * (κονσόλα)
+ * @author kosta
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello world!");
+        System.out.println();
+    }
+}
